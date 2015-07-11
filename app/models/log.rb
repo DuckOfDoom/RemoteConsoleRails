ff6ef8a0844c3e46a7296369960cf433ab5b2078
DuckOfDoom <horaxlfa@gmail.com>
2015-07-11 22:02:53 +0300

    Refactored column search

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,9 +1,9 @@
 class Log < ActiveRecord::Base
-    def self.search(search)
-        if search
-            where('log LIKE ?', "%#{search}%")
+    def self.search(search_column, search_pattern)
+        if search_column && search_pattern
+            where(search_column + ' LIKE ?', "%#{search_pattern}%")
         else
-            scoped
+            where(nil)
         end
     end
 end
