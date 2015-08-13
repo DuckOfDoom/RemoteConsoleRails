43cfa4a60b9c13715c5e7d70b71ea560abe28cd5
DuckOfDoom <horaxlfa@gmail.com>
2015-08-13 18:21:07 +0300

    Fixed formatting issues with newlines

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -6,4 +6,8 @@ module LogsHelper
     def format_timestamp(time)
         time.strftime("%k:%M:%S, %-d %b %Y")
     end
+    
+    def format_short_log(log)
+        simple_format log.lines.first(2).join("\n")
+    end
 end
